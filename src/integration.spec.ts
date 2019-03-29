359ea843a61f2dad602ac80655e31b79c58582f1
Suguru Inatomi <suguru.inatomi@gmail.com>
2019-03-29 17:14:39 +0900

    test: add specs for immer

---
+++ src/integration.spec.ts
@@ -0,0 +1,33 @@
+import { Store } from './store';
+import produce from 'immer';
+
+describe('integration specs', () => {
+  describe('with immer', () => {
+    type State = { count: number };
+    let store: Store<State>;
+
+    beforeEach(() => {
+      store = new Store<State>({
+        initialValue: { count: 0 },
+      });
+    });
+
+    it('should work well with `produce`', () => {
+      store.update(value =>
+        produce(value, draft => {
+          draft.count = 5;
+        }),
+      );
+      expect(store.value.count).toBe(5);
+    });
+
+    it('should work well with curried `produce`', () => {
+      store.update(
+        produce(draft => {
+          draft.count = 5;
+        }),
+      );
+      expect(store.value.count).toBe(5);
+    });
+  });
+});
