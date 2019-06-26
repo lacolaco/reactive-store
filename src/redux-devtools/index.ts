54d632e7b2dff449d8befba4178b7c01e49d7ce3
Suguru Inatomi <suguru.inatomi@gmail.com>
2019-06-26 23:49:54 +0900

    fix(redux-devtools): init devtools on connect

--- src/redux-devtools/index.ts
+++ src/redux-devtools/index.ts
@@ -5,12 +5,14 @@ const globalExtensionSymbol = '__REDUX_DEVTOOLS_EXTENSION__';
 type ReduxDevtools = {
   send(action: string, state: any): void;
   subscribe(onMessage: (message: any) => void): void;
+  init(state: any): void;
 };
 
 export function connectReduxDevTools(store: Store<any>) {
   const extension = (window as any)[globalExtensionSymbol] as any;
   if (extension) {
     const reduxDevTool = extension.connect() as ReduxDevtools;
+    reduxDevTool.init(store.value);
     store.storeUpdateChanges.subscribe(change => {
       reduxDevTool.send(change.label || 'Update State', change.currentValue);
     });
